cca457399503eb9c5146777119a6f364784ae9fd
NguyenMui <a746859@gmail.com>
2024-10-05 11:02:42 +0700

    lecture2

---
+++ lecture2.java
@@ -0,0 +1,22 @@
+public class lecture2 {
+   
+ public static void main(String[] args)
+ {
+    double distance, mass;
+
+    distance = 1.495979E11;
+    mass = 1.989E30;
+    System.out.println("The sun is " + distance +
+    " meters away.");
+    System.out.println("The sun's mass is " + mass +
+    " kilograms.");
+
+    char letter;
+    letter = 85;
+    System.out.println(letter);
+    letter = 66;
+    System.out.println(letter);
+}
+ }
+
+
